79a8f7b034055a3b8d47c543395791fd635df4fb
Maykoo Cunha <maykoocunha@gmail.com>
2025-06-05 01:19:05 -0300

    Update Funcoes.java

--- src/control/Funcoes.java
+++ src/control/Funcoes.java
@@ -1,6 +1,34 @@
-package src.control;
+package Dao;
 
-public class Funcoes {
-    // Crie aqui as funções que irão realizar a comunicação com o banco de dados
-    // Faça como métodos estáticos
+import java,sql.Connection;
+
+public class Dao {
+
+	private Connection con; 
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/refrigeradbq";
+	private String user = "root";
+	private String password = "143094";
+	
+	public Connection conectar() {
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password); 
+			return con;
+		} catch	(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+		
+	}
+
+}
+public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            System.err.println("ERRO NA CONEXÃO:");
+            throw new RuntimeException("Falha na conexão", e);
+        }
+    }
 }
